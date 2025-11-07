b0cff451bd58e7a3509da20013e0afea8f7cf479
BasileLT <blt_git@pm.me>
2025-11-07 11:18:40 +0100

    added extra labels

--- birds/src/main/java/ensisa/birds/MainController.java
+++ birds/src/main/java/ensisa/birds/MainController.java
@@ -7,14 +7,20 @@ import javafx.scene.control.Label;
 
 public class MainController {
     private BirdRepository repository;
+    public Bird currentBird;
 
     @FXML
     private Label commonNameLabel;
-
     @FXML
     private Label latinNameLabel;
-
-    public Bird currentBird;
+    @FXML
+    private Label familyLabel;
+    @FXML
+    private Label genusLabel;
+    @FXML
+    private Label specieLabel;
+    @FXML
+    private Label descriptionLabel;
 
     public MainController() {
         repository = new BirdRepository();
@@ -26,5 +32,9 @@ public class MainController {
     public void initialize() {
         commonNameLabel.textProperty().bind(currentBird.commonNameProperty());
         latinNameLabel.textProperty().bind(currentBird.latinNameProperty());
+        familyLabel.textProperty().bind(currentBird.familyProperty());
+        genusLabel.textProperty().bind(currentBird.genusProperty());
+        specieLabel.textProperty().bind(currentBird.specieProperty());
+        descriptionLabel.textProperty().bind(currentBird.descriptionProperty());
     }
 }
