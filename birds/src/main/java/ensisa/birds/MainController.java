202989cf1b4ef4871dc73ec97cf01a2ae57ed526
BasileLT <blt_git@pm.me>
2025-11-07 10:40:34 +0100

    hello as placeholder

--- birds/src/main/java/ensisa/birds/MainController.java
+++ birds/src/main/java/ensisa/birds/MainController.java
@@ -1,3 +1,14 @@
+package ensisa.birds;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class MainController {
-    
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
 }
