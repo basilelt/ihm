13a7e316d107de302db45e1ad805d477a62064d0
BasileLT <blt_git@pm.me>
2025-11-07 11:07:05 +0100

    added labels to controller

--- birds/src/main/java/ensisa/birds/MainController.java
+++ birds/src/main/java/ensisa/birds/MainController.java
@@ -1,12 +1,29 @@
 package ensisa.birds;
 
+import ensisa.birds.model.Bird;
 import ensisa.birds.model.BirdRepository;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class MainController {
     private BirdRepository repository;
 
+    @FXML
+    private Label commonNameLabel;
+
+    @FXML
+    private Label latinNameLabel;
+
+    public Bird currentBird;
+
     public MainController() {
         repository = new BirdRepository();
         repository.load();
+        currentBird = repository.birds.get(0);
+    }
+
+    public void initialize() {
+        commonNameLabel.setText(currentBird.getCommonName());
+        latinNameLabel.setText(currentBird.getLatinName());
     }
 }
