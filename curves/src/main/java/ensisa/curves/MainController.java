5820f86128cba7d8d636ae71ca08e0af0ccbaebc
BasileLT <blt_git@pm.me>
2025-12-08 17:00:46 +0100

    init fxml

--- curves/src/main/java/ensisa/curves/MainController.java
+++ curves/src/main/java/ensisa/curves/MainController.java
@@ -1,8 +1,13 @@
 package ensisa.curves;
 
+import javafx.application.*;
 import javafx.fxml.*;
 import javafx.scene.control.*;
 
 public class MainController {
+    @FXML
+    private void quitMenuAction() {
+        Platform.exit();
+    }
 
 }
